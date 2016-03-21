c48486ceeebf14897cf85a506562a601ba9fc2e1
张志豪 <izhangzhihao@hotmail.com>
2016-03-21 21:14:57 +0800

    简单工厂模式

---
+++ SimpleFactory/Operation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    public class Operation
+    {
+        private double _numberA;
+
+        public double NumberA
+        {
+            get { return _numberA; }
+            set
+            {
+                try
+                {
+                    _numberA = Convert.ToDouble(value);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        private double _numberB;
+
+        public double NumberB
+        {
+            get { return _numberB; }
+            set
+            {
+                try
+                {
+                    _numberB = Convert.ToDouble(value);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        public virtual double GetResult()
+        {
+            double result = 0.0;
+            return result;
+        }
+    }
+}
---
+++ SimpleFactory/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string oper = string.Empty;
+            Operation operation=new Operation();
+            Console.Title = "简单工厂模式";
+            Console.WriteLine("加减乘除：+-*/");
+            Console.WriteLine("请输入运算法则：");
+            oper = Console.ReadKey().KeyChar.ToString();
+            operation = OperationFactory.CreateOperation(oper);
+            Console.WriteLine("\n请输入第一个数字：");
+            operation.NumberA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入第二个数字：");
+            operation.NumberB = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("最后的结果是" + operation.GetResult()+"按任意键结束");
+            Console.ReadKey();
+        }
+    }
+}
