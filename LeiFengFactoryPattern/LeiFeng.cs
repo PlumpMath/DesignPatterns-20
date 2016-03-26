328bea8a59c75ab6964d0794913b9a9084ec18c8
张志豪 <izhangzhihao@hotmail.com>
2016-03-26 08:55:21 +0800

    增加工厂模式

---
+++ LeiFengFactoryPattern/LeiFeng.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeiFengFactoryPattern
+{
+    public class LeiFeng
+    {
+        public void Sweep()
+        {
+            Console.WriteLine("扫地");
+        }
+
+        public void Wash()
+        {
+            Console.WriteLine("洗衣服");
+        }
+
+        public void BuyRice()
+        {
+            Console.WriteLine("买米");
+        }
+    }
+}
---
+++ LeiFengFactoryPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeiFengFactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFactory factory=new UndergraduateFactory();
+            LeiFeng student = factory.CreateLeiFeng();
+            student.BuyRice();
+            student.Sweep();
+            student.Wash();
+            Console.ReadKey();
+
+
+            IFactory _factory=new VolunteerFactory();
+            LeiFeng leifengVolunteer = _factory.CreateLeiFeng();
+            leifengVolunteer.BuyRice();
+            leifengVolunteer.Sweep();
+            leifengVolunteer.Wash();
+            Console.ReadKey();
+        }
+    }
+}
