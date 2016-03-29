51c0b173221dc429586d7f1373ae1a06603479ff
张志豪 <izhangzhihao@hotmail.com>
2016-03-29 11:19:25 +0800

    增加外观模式

---
+++ FacadePattern/Facade.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    public class Facade
+    {
+        SubSystemOne one;
+        SubSystemTwo two;
+        SubSystemThree three;
+        SubSystemFour four;
+
+        public Facade()
+        {
+            one=new SubSystemOne();
+            two=new SubSystemTwo();
+            three=new SubSystemThree();
+            four=new SubSystemFour();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("MethodA is start");
+            one.MethodOne();
+            two.MethodTwo();
+            three.MethodThree();
+            four.MethodFour();
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("MethodB is start");
+            one.MethodOne();
+            two.MethodTwo();
+            three.MethodThree();
+            four.MethodFour();
+        }
+    }
+}
---
+++ FacadePattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade f=new Facade();
+            f.MethodA();
+            f.MethodB();
+            Console.ReadKey();
+        }
+    }
+}
