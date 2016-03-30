deed3d28366cefb30360be3de75d1597f356a2c2
张志豪 <izhangzhihao@hotmail.com>
2016-03-30 15:15:35 +0800

    增加建造者模式

---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Director d=new Director();
+            Builder b1=new ConcreteBuilder1();
+            Builder b2=new ConcreteBuilder2();
+
+            d.Construct(b1);
+            Product p1 = b1.GetProduct();
+            p1.Show();
+
+            d.Construct(b2);
+            Product p2 = b2.GetProduct();
+            p2.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
