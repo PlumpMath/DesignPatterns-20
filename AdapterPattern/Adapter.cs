b9c43e2b9a085a974bbf075e5f39df28e2e13ce8
张志豪 <izhangzhihao@hotmail.com>
2016-04-10 09:38:43 +0800

    增加适配器模式

---
+++ AdapterPattern/Adapter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class Adapter : Target
+    {
+        //private Adaptee adaptee=new Adaptee();
+        private Adaptee _adaptee;
+
+        public Adaptee Adaptee
+        {
+            get
+            {
+                return _adaptee;
+            }
+
+            set
+            {
+                _adaptee = value;
+            }
+        }
+
+        public Adapter(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public override void Request()
+        {
+            //base.Request();
+            _adaptee.SpecificRequest();
+        }
+    }
+}
---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Target target=new Target();
+            //target.Request();
+            //Console.ReadKey();
+            
+            //Adaptee adaptee=new Adaptee();
+            //Adapter adapter=new Adapter(adaptee);
+            //adapter.Request();
+            //Console.ReadKey();
+
+
+            Target target=new Adapter(new Adaptee());
+            target.Request();
+            Console.ReadKey();
+        }
+    }
+}
