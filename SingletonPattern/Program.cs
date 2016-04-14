7671dfd270e179fe272f3b7b56dd41e872bce7d5
张志豪 <izhangzhihao@hotmail.com>
2016-04-14 09:35:18 +0800

    增加单例模式

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //错误，不应调用构造函数
+            //Singleton s1=new Singleton();
+            //Singleton s2=new Singleton();
+            //if (s1 == s2)
+            //{
+            //    Console.WriteLine("两个对象相同");
+            //}
+
+
+            //单线程下
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+            if (s1 == s2)
+            {
+                Console.WriteLine("两个对象相同");
+            }
+
+            //单线程
+            SealedSingleton ss1=SealedSingleton.GetInstance();
+            SealedSingleton ss2 = SealedSingleton.GetInstance();
+            if (ss1 == ss2)
+            {
+                Console.WriteLine("两个对象相同");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static readonly object syncRoot=new object();
+        private static Singleton _instance=null;
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+
+            set
+            {
+                _instance = value;
+            }
+        }
+
+        public static Singleton GetInstance()
+        {
+            //不行
+            //if (_instance != null)
+            //{
+            //    return _instance;
+            //}
+            //else
+            //{
+            //    return new Singleton();
+            //}
+
+
+            //不行
+            //return Instance ?? new Singleton();
+
+
+            //可以
+
+            //if (_instance != null)
+            //{
+            //    return _instance;
+            //}
+            //else
+            //{
+            //    _instance= new Singleton();
+            //    return _instance;
+            //}
+
+
+
+
+            //可以
+            //if (Instance == null)
+            //{
+            //    Instance = new Singleton();
+            //}
+            //return Instance;
+
+
+            //可以
+            //return Instance ?? (Instance = new Singleton());//要赋值才可以
+
+
+
+
+            //多线程加锁
+
+            //if (Instance == null)
+            //{
+            //    lock (syncRoot)
+            //    {
+            //        if (Instance == null)
+            //        {
+            //            Instance=new Singleton();
+            //        }
+            //    }
+            //}
+            //return Instance;
+
+
+            if (Instance != null) return Instance;
+            lock (syncRoot)
+            {
+                if (Instance == null)
+                {
+                    Instance = new Singleton();
+                }
+            }
+            return Instance;
+
+        }
+    }
+}
